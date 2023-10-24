d30721db8f355b7b7f12d9159074757219bba022
Aravind Venkatapathy <arsenal.aravind@gmail.com>
2023-10-24 16:20:30 -0400

    including file input

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.util.Scanner;
@@ -10,19 +14,39 @@ public class Client extends AbstractClientFunctionClass{
       Registry registry = LocateRegistry.getRegistry(serverAddress, clientPort);
       RMIServer stub = (RMIServer) registry.lookup("RMIServer");
       System.out.println(getCurrentTime()+" Client is running");
-      System.out.println(getCurrentTime()+" This is a menu driven program with the following commands: PUT/GET/DELETE");
+      System.out.println(getCurrentTime()+" This is a menu driven program with the following commands: PUT/GET/DELETE/file");
       System.out.println(getCurrentTime()+" Enter 'exit' to exit");
       Scanner sc = new Scanner(System.in);
       while (true) {
         String clientMessage = "";
-        clientMessage+=sc.nextLine();
+        clientMessage += sc.nextLine();
+        switch (clientMessage.toUpperCase()) {
+          case "FILE": {
+            File file = new File("commands.txt");
+            try (FileInputStream fileInputStream = new FileInputStream(file);
+                 InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                 BufferedReader reader = new BufferedReader(inputStreamReader)) {
+              String line;
+              while ((line = reader.readLine()) != null) {
+                String toServer = clientRead(line);
+                String serverResponse = stub.redirectingRequests(toServer, "", serverAddress, String.valueOf(clientPort));
+                System.out.println(getCurrentTime() + " Received from server: " + serverResponse);
+              }
+              break;
+            }
+          }
+          case "EXIT" :{
+            System.out.println(getCurrentTime() + " Client disconnected");
+            break;
+          } default: {
+            String toServer = clientRead(clientMessage);
+            String serverResponse = stub.redirectingRequests(toServer, "", serverAddress, String.valueOf(clientPort));
+            System.out.println(getCurrentTime() + " Received from server: " + serverResponse);
+          }
+        }
         if (clientMessage.equalsIgnoreCase("exit")) {
-          System.out.println(getCurrentTime()+" Client disconnected");
           break;
         }
-        String toServer = clientRead(clientMessage);
-        String serverResponse = stub.redirectingRequests(toServer,"",serverAddress,String.valueOf(clientPort));
-        System.out.println(getCurrentTime()+" Received from server: "+serverResponse);
       }
     }
     catch (Exception e){
